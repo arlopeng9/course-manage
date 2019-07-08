60782b03c61a16452e6d8da5d3837cc1bb27d4d9
arlopeng9 <45678638+arlopeng9@users.noreply.github.com>
2019-07-08 19:55:21 +0800

    123

--- test/model_predict.php
+++
@@ -1,5 +0,0 @@
-<?php
-$data = "每次都去，老师会讽刺小组展示，感觉不尊重人。照本宣科，课程无聊";
-$data = urlencode($data);
-exec("python E:/PHP/htdocs/test/python/model_predict.py {$data}", $out, $res);
-print($out[0]);
--- test/sensitive_words_analysis.php
+++
@@ -1,6 +0,0 @@
-<?php
-$data = "这门课考试作弊很简单；法轮功是邪教";
-$data = urlencode($data);
-exec("python  E:/PHP/htdocs/test/python/sensitive_words_analysis.py {$data}", $out, $res);
-print($out[0]);
-?>
